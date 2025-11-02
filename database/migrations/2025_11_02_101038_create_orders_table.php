bd92c80fad5374fa65161e09a18c624150fa8c9d
Helmy Krisdin Garcia <helmygarcia67@gmail.com>
2025-11-02 17:24:59 +0700

    typo

--- database/migrations/2025_11_02_101038_create_orders_table.php
+++ database/migrations/2025_11_02_101038_create_orders_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Shema::create('orders', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->datetime('order_date');
